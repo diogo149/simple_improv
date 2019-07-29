ebea0878e7afc5f5929eef35d7b871bb4678a26b
Diogo Moitinho de Almeida <diogo149@gmail.com>
2019-07-29 09:22:29 -0500

    Updates

---
+++ precache-manifest.e70d4ee7bcd5b1a4cfefdd57ca49cceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02a18efc7d71c4dcbd419f5733c37e4",
+    "url": "/simple_improv/index.html"
+  },
+  {
+    "revision": "9db996ce0f8f19807ce9",
+    "url": "/simple_improv/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/simple_improv/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9db996ce0f8f19807ce9",
+    "url": "/simple_improv/static/js/main.7a63257e.chunk.js"
+  },
+  {
+    "revision": "f44ee33b1c2519aab1d0",
+    "url": "/simple_improv/static/js/runtime~main.a0680db1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple_improv/static/media/logo.5d5d9eef.svg"
+  }
+]);
