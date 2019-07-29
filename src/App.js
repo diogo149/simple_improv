f5034ff45dbc976535c2f755bc30ad615e2c97c4
Diogo Moitinho de Almeida <diogo149@gmail.com>
2019-07-29 09:45:05 -0500

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Emotions from './constants/Emotions';
+import CanIGetA from './constants/CanIGetA';
 
-function App() {
+
+function randomItem(items) {
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+function OldApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +25,31 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	Emotion (1): {randomItem(Emotions.level1)}
+	<br/>
+	<br/>
+	Emotion (2): {randomItem(Emotions.level2)}
+	<br/>
+	<br/>
+	Emotion (3): {randomItem(Emotions.level3)}
+	<br/>
+	<br/>
+	Location: {randomItem(CanIGetA.locations)}
+	<br/>
+	<br/>
+	Relationship: {randomItem(CanIGetA.relationships)}
+	<br/>
+	<br/>
+	Word: {randomItem(CanIGetA.words)}
       </header>
     </div>
   );
---
+++ src/constants/Emotions.js
@@ -0,0 +1,63 @@
+// https://www.thejuntoinstitute.com/blog-posts/the-junto-emotion-wheel-why-and-how-we-use-it
+
+const emotions = {
+  "love": {
+    "peaceful": ["satisfied, tranquil"],
+    "tender": ["compassionate", "caring"],
+    "longing": ["affectionate", "sentimental"],
+    "romantic": ["enamored", "attractive"],
+    "desirous": ["infatuated", "passionate"]
+  },
+  "joy": {
+    "enthralled": ["rapturous", "enchanted"],
+    "elated": ["jubilant", "euphoric"],
+    "enthusiastic": ["zealous", "excited"],
+    "optimistic": ["hopeful", "eager"],
+    "proud": ["illustrious", "triumphant"],
+    "cheerful": ["playful", "jovial"],
+    "happy": ["delighted", "amused"],
+    "content": ["pleased", "thankful"]
+  },
+  "surprise": {
+    "moved": ["touched", "stimulated"],
+    "overcome": ["astounded", "speechless"],
+    "amazed": ["awe-struck", "astonished"],
+    "confused": ["disillusioned", "perplexed"],
+    "stunned": ["shocked", "dismayed"]
+  },
+  "sadness": {
+    "hopeless": ["anguised", "powerless"],
+    "neglected": ["isolated", "lonely"],
+    "shameful": ["regretful", "guilty"],
+    "disappointed": ["dismayed", "displeased"],
+    "melancholic": ["depressed", "sorrowful"],
+    "distressed": ["agonized", "hurt"]
+  },
+  "anger": {
+    "disgusted": ["contemptuous", "revolted"],
+    "envious": ["resentful", "jealous"],
+    "irritable": ["annoyed", "aggravated"],
+    "exasperated": ["agitated", "frustrated"],
+    "enraged": ["hateful", "hostile"]
+  },
+  "fear": {
+    "horrified": ["mortified", "dreadful"],
+    "nervous": ["worried", "anxious"],
+    "insecure": ["inferior", "inadequate"],
+    "terrified": ["panic", "hysterical"],
+    "scared": ["helpless", "frightened"]
+  }
+};
+
+const level1 = Object.keys(emotions);
+
+const level2 = Object.values(emotions).flatMap(x => Object.keys(x));
+
+const level3 = Object.values(emotions).flatMap(x => Object.values(x).flatMap(x => x));
+
+export default {
+  emotions,
+  level1,
+  level2,
+  level3
+};
