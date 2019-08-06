db35c9732f7d9a303c7ea45ab77c77e4a488faf4
Diogo Moitinho de Almeida <diogo149@gmail.com>
2019-08-05 22:15:35 -0500

    more choices

--- src/constants/CustomChoices.js
+++ src/constants/CustomChoices.js
@@ -28,7 +28,8 @@ const secretWants = [
   "to be serenaded",
   "be seen as funny",
   "apology",
-  "forgiveness"
+  "forgiveness",
+  "a sandwich"
 ];
 
 const firstNames = [
@@ -45,7 +46,11 @@ const firstNames = [
   "Beauregard",
   "William",
   "Albert",
+  "Tanner",
+  "Wesley",
 
+  "Alex",
+  "Sasha",
   "Sam",
   "Jordan",
   "Morgan",
@@ -61,8 +66,129 @@ const firstNames = [
   "Ellen"
 ];
 
+const lastNames = [
+  "Rogers",
+  "Murphy",
+  "Payton"
+];
+
+// http://www.soulcraft.co/essays/the_12_common_archetypes.html
+const jungArchetypes = [
+  // Ego
+  "Innocent: Free to be you and me",
+  "Everyman: All are created equal",
+  "Hero: Where there's a will, there's a way",
+  "Caregiver: Love your neighbor as yourself",
+  // Soul
+  "Explorer: Don't fence me in",
+  "Rebel: Rules are made to be broken",
+  "Lover: You're the only one",
+  "Creator: If you can imagine it, it can be done",
+  // Self
+  "Jester: You only live once",
+  "Sage: The truth will set you free",
+  "Magician: I make things happen",
+  "Rules: Power isn't everything, it's the only thing"
+];
+
+// https://en.wikipedia.org/wiki/List_of_stock_characters
+// $(".wikitable td:first-child").map((x, y) => $(y).text())
+const wikiStockCharacters = [
+  "Absent-minded professor",
+  // "Angry Black Woman",
+  "Antihero",
+  "Author surrogate",
+  "Bad boy",
+  "Battle-axe",
+  "Black knight",
+  "Boy next door",
+  "Bug-eyed monster",
+  "Cat lady",
+  "Contender",
+  "Criminal",
+  "Crone",
+  "Damsel in distress",
+  "Dandy",
+  "Dark Lady",
+  "Dark Lord",
+  "Elderly martial arts master",
+  "Everyman",
+  "Fall guy",
+  "Farmer's daughter",
+  "Femme fatale",
+  "Final girl",
+  "Gentleman thief",
+  "Girl next door",
+  "Grande dame",
+  "Hag",
+  // "Harlequin",
+  "Clown",
+  "Hooker with a heart of gold",
+  "Hopeless Romantic",
+  "Hotshot",
+  "Idiot savant",
+  "Ingenue",
+  "Jock (athlete)",
+  "Knight-errant",
+  "Little Green Men",
+  "Loathly lady",
+  "Lovers",
+  "Mad scientist",
+  // "Magical Negro",
+  "Magician",
+  "Mammy archetype",
+  "Manic Pixie Dream Girl",
+  // "Mary Sue",
+  // "Miles Gloriosus",
+  "Boastful soldier",
+  "Mother's boy",
+  "Nerd",
+  "Nice guy",
+  "Noble savage",
+  "Outlaw",
+  // "Pantomime dame",
+  // "Petrushka",
+  "Jester",
+  // "Pierrot",
+  "Pantomime",
+  "Princesse lointaine",
+  "Redshirt",
+  "Rightful king",
+  "Senex iratus",
+  "Shrew",
+  "Sinnekins",
+  "Soubrette",
+  "Southern belle",
+  // "Space Nazis",
+  "Spear carrier",
+  "Straight man",
+  "Superhero",
+  "Supersoldier",
+  "Supervillain",
+  "Swashbuckler",
+  "Tomboy",
+  "Tortured artist",
+  "Town drunk",
+  "Tragic hero",
+  // "Tragic mulatto",
+  "Übermensch",
+  "Vice",
+  "Village idiot",
+  "Villain",
+  "Whisky priest",
+  // "White hunter",
+  "Hunter",
+  "Wise fool",
+  "Wise old man",
+  "Yokel",
+  // "Youxia",
+];
+
 export default {
   props,
   secretWants,
-  firstNames
+  firstNames,
+  lastNames,
+  jungArchetypes,
+  wikiStockCharacters
 };
